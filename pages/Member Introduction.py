d0e235a844136c99acca4f4f6086a36331299de7
Eso-o <133123562+Eso-o@users.noreply.github.com>
2023-06-27 14:09:46 +0800

    Update Member Introduction.py

--- pages/Member Introduction.py
+++ pages/Member Introduction.py
@@ -7,8 +7,7 @@ with tab1:
     st.write("*Patience is the key in life*")
     st.divider()
     image1 = Image.open('t2.jpg')
-    image1=image1.resize(20,20)
-    st.image(image1, caption="Chief Programmer, responsible for distribution and regional analysis webs")
+    st.image(image1,width=20, caption="Chief Programmer, responsible for distribution and regional analysis webs")
     with st.expander("Show source code"):
         code = '''
 import streamlit as st
@@ -37,8 +36,7 @@ with tab2:
     st.write("*Python is the key in life*")
     st.divider()
     image2 = Image.open('t3.jpg')
-    image2=image2.resize(20,20)
-    st.image(image2, caption="Programmer, mainly responsible for the personal analysis module")
+    st.image(image2, width=20,caption="Programmer, mainly responsible for the personal analysis module")
     with st.expander("Show source code"):
         code = '''
 import streamlit as st
