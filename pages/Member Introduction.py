c232779db1b9d368428402389fbf7b3b07e47e90
Eso-o <133123562+Eso-o@users.noreply.github.com>
2023-06-27 13:58:17 +0800

    Update Member Introduction.py

--- pages/Member Introduction.py
+++ pages/Member Introduction.py
@@ -6,12 +6,50 @@ with tab1:
     st.subheader('**:blue[Qitong Xu]**')
     st.write("*Patience is the key in life*")
     st.divider()
-    image1 = Image.open('t2.jpg',width=20)
+    image1 = Image.open('t2.jpg')
+    image1.resize(20,20)
     st.image(image1, caption="Chief Programmer, responsible for distribution and regional analysis webs")
+    with st.expander("Show source code"):
+        code = '''
+import streamlit as st
+from PIL import Image
+tab1, tab2 = st.tabs(["Qitong Xu", "Yu Chen"])
+with tab1:
+    st.title('**Member Introduction** :blush:')
+    st.subheader('**:blue[Qitong Xu]**')
+    st.write("*Patience is the key in life*")
+    st.divider()
+    image1 = Image.open('t2.jpg')
+    image1.resize(20,20)
+    st.image(image1, caption="Chief Programmer, responsible for distribution and regional analysis webs")
+    '''
 with tab2:
     st.title('**Member Introduction** :blush:')
     st.subheader('**:orange[Yu Chen]**')
     st.write("*Python is the key in life*")
     st.divider()
-    image2 = Image.open('t3.jpg',width=20)
+    image2 = Image.open('t3.jpg')
+    image2.resize(20,20)
     st.image(image2, caption="Programmer, mainly responsible for the personal analysis module")
+with tab2:
+    st.title('**Member Introduction** :blush:')
+    st.subheader('**:orange[Yu Chen]**')
+    st.write("*Python is the key in life*")
+    st.divider()
+    image2 = Image.open('t3.jpg')
+    image2.resize(20,20)
+    st.image(image2, caption="Programmer, mainly responsible for the personal analysis module")
+    with st.expander("Show source code"):
+        code = '''
+import streamlit as st
+from PIL import Image
+tab1, tab2 = st.tabs(["Qitong Xu", "Yu Chen"])
+with tab1:
+    st.title('**Member Introduction** :blush:')
+    st.subheader('**:blue[Qitong Xu]**')
+    st.write("*Patience is the key in life*")
+    st.divider()
+    image1 = Image.open('t2.jpg')
+    image1.resize(20,20)
+    st.image(image1, caption="Chief Programmer, responsible for distribution and regional analysis webs")
+    '''
