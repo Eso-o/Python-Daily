598f70c2046fc8611906ee75bc6f137200c255af
Eso-o <133123562+Eso-o@users.noreply.github.com>
2023-06-27 13:34:06 +0800

    Add files via upload

---
+++ pages/Member Introduction.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from PIL import Image
+tab1, tab2 = st.tabs(["Qitong Xu", "Yu Chen"])
+with tab1:
+    st.title('**Member Introduction** :blush:')
+    st.subheader('**:blue[Qitong Xu]**')
+    image = Image.open('t2.jpeg')
+    st.image(image, caption="Chief Programmer, responsible for distribution and regional analysis webs")
+    st.divider()
+    st.write("*Patience is the key in life*")
+with tab2:
+    st.title('**Member Introduction** :blush:')
+    st.subheader('**:blue[Qitong Xu]**')
+    image = Image.open('t3.jpeg')
+    st.image(image, caption="Programmer, mainly responsible for the personal analysis module")
+    st.divider()
+    st.write("*Python is the key in life*")
