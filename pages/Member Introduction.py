69a380e84f25d5a2e73ab6898e1265e7ca9dd0f8
Eso-o <133123562+Eso-o@users.noreply.github.com>
2023-06-27 14:13:06 +0800

    Update Member Introduction.py

--- pages/Member Introduction.py
+++ pages/Member Introduction.py
@@ -8,28 +8,6 @@ with tab1:
     st.divider()
     image1 = Image.open('t2.jpg')
     st.image(image1,width=20, caption="Chief Programmer, responsible for distribution and regional analysis webs")
-    with st.expander("Show source code"):
-        code = '''
-import streamlit as st
-from PIL import Image
-tab1, tab2 = st.tabs(["Qitong Xu", "Yu Chen"])
-with tab1:
-    st.title('**Member Introduction** :blush:')
-    st.subheader('**:blue[Qitong Xu]**')
-    st.write("*Patience is the key in life*")
-    st.divider()
-    image1 = Image.open('t2.jpg')
-    image1=image1.resize(20,20)
-    st.image(image1, caption="Chief Programmer, responsible for distribution and regional analysis webs")
-with tab2:
-    st.title('**Member Introduction** :blush:')
-    st.subheader('**:orange[Yu Chen]**')
-    st.write("*Python is the key in life*")
-    st.divider()
-    image2 = Image.open('t3.jpg')
-    image2=image2.resize(20,20)
-    st.image(image2, caption="Programmer, mainly responsible for the personal analysis module")
-    '''
 with tab2:
     st.title('**Member Introduction** :blush:')
     st.subheader('**:orange[Yu Chen]**')
@@ -37,7 +15,7 @@ with tab2:
     st.divider()
     image2 = Image.open('t3.jpg')
     st.image(image2, width=20,caption="Programmer, mainly responsible for the personal analysis module")
-    with st.expander("Show source code"):
+with st.expander("Show source code"):
         code = '''
 import streamlit as st
 from PIL import Image
@@ -59,3 +37,4 @@ with tab2:
     image2=image2.resize(20,20)
     st.image(image2, caption="Programmer, mainly responsible for the personal analysis module")
     '''
+        st.code(code,language='python')
