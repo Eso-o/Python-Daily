a5ff098ee8f14693c875867a533471117e8fa720
Eso-o <133123562+Eso-o@users.noreply.github.com>
2023-06-27 14:06:05 +0800

    Update Member Introduction.py

--- pages/Member Introduction.py
+++ pages/Member Introduction.py
@@ -7,7 +7,7 @@ with tab1:
     st.write("*Patience is the key in life*")
     st.divider()
     image1 = Image.open('t2.jpg')
-    image1.resize(20,20)
+    image1=image1.resize(20,20)
     st.image(image1, caption="Chief Programmer, responsible for distribution and regional analysis webs")
     with st.expander("Show source code"):
         code = '''
@@ -20,7 +20,7 @@ with tab1:
     st.write("*Patience is the key in life*")
     st.divider()
     image1 = Image.open('t2.jpg')
-    image1.resize(20,20)
+    image1=image1.resize(20,20)
     st.image(image1, caption="Chief Programmer, responsible for distribution and regional analysis webs")
 with tab2:
     st.title('**Member Introduction** :blush:')
@@ -28,7 +28,7 @@ with tab2:
     st.write("*Python is the key in life*")
     st.divider()
     image2 = Image.open('t3.jpg')
-    image2.resize(20,20)
+    image2=image2.resize(20,20)
     st.image(image2, caption="Programmer, mainly responsible for the personal analysis module")
     '''
 with tab2:
@@ -37,7 +37,7 @@ with tab2:
     st.write("*Python is the key in life*")
     st.divider()
     image2 = Image.open('t3.jpg')
-    image2.resize(20,20)
+    image2=image2.resize(20,20)
     st.image(image2, caption="Programmer, mainly responsible for the personal analysis module")
     with st.expander("Show source code"):
         code = '''
@@ -50,7 +50,7 @@ with tab1:
     st.write("*Patience is the key in life*")
     st.divider()
     image1 = Image.open('t2.jpg')
-    image1.resize(20,20)
+    image1=image1.resize(20,20)
     st.image(image1, caption="Chief Programmer, responsible for distribution and regional analysis webs")
 with tab2:
     st.title('**Member Introduction** :blush:')
@@ -58,6 +58,6 @@ with tab2:
     st.write("*Python is the key in life*")
     st.divider()
     image2 = Image.open('t3.jpg')
-    image2.resize(20,20)
+    image2=image2.resize(20,20)
     st.image(image2, caption="Programmer, mainly responsible for the personal analysis module")
     '''
